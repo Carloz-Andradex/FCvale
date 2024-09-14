4db65e8c382d43686d589f53a15ea004ddd5993c
Carloz-Andradex <andradecarlos2503@hotmail.com>
2024-09-14 01:51:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function showMessage() {
-    document.getElementById('surprise').classList.toggle('hidden');
-    setTimeout(function() {
-        window.location.href = 'https://www.youtube.com/watch?v=z6L8q62AfBM';
-    }, 30000);
+    const surprise = document.getElementById('surprise');
+    surprise.classList.toggle('hidden');
+    setTimeout(() => {
+        window.location.href = 'https://www.youtube.com/watch?v=z6L8q62AfBM'; // Cambia esta URL por la que desees
+    }, 30000); // Redirige después de 30 segundos
 }
