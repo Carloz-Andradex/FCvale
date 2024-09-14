bc0b39cdfc96c1ed9b54b72b877640148476fc3f
Carloz-Andradex <andradecarlos2503@hotmail.com>
2024-09-14 01:39:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function showMessage() {
     document.getElementById('surprise').classList.toggle('hidden');
     setTimeout(function() {
         window.location.href = 'https://www.youtube.com/watch?v=z6L8q62AfBM';
-    }, 15000);
+    }, 30000);
 }
